1dc9346c02b2f95b5fa7a7293cfb917252d7ff97
femiadefila <liladeoluwa@gmail.com>
2023-12-18 13:49:27 +0100

    changes

--- script.js
+++ script.js
@@ -16,23 +16,17 @@ window.addEventListener("scroll", showScroll);
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
-const menuItems = document.querySelectorAll('nav .mainMenu li a');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
 
-// close menu when you click on a menu item
-menuItems.forEach(item => {
-  item.addEventListener('click', function () {
-    close();
-  });
-});
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
 
-function show() {
-  mainMenu.style.display = 'flex';
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+    console.log(show())
 }
-
-function close() {
-  mainMenu.style.display = 'none';
+function close(){
+    mainMenu.style.top = '-100%';
 }
-
